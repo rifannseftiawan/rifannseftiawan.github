ed0101f68f8abcc8b0aeae02a0287cb1218802c0
rifannseftiawan <rseftiawan909@gmail.com>
2023-11-07 22:20:02 -0800

    first commit

---
+++ halaman.js
@@ -0,0 +1,5 @@
+document.getElementById("masukButton").addEventListener("click", function() {
+    // Ganti URL halaman berikutnya sesuai kebutuhan
+    window.location.href = "spesial.html";
+  });
+  
